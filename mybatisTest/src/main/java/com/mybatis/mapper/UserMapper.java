f86f5d4c60d9bd1d60239e7817988bc19e97ade4
leilvlong <1455013965@qq.com>
2019-05-20 20:06:33 +0800

    mybatis 基本配置使用

--- mybatisTest/src/main/java/com/mybatis/mapper/UserMapper.java
+++ mybatisTest/src/main/java/com/mybatis/mapper/UserMapper.java
@@ -1,13 +1,20 @@
 package com.mybatis.mapper;
 
+import com.mybatis.domain.Content;
 import com.mybatis.domain.User;
 
 import java.util.List;
 
 public interface UserMapper {
-    public List<User> findAll();
-
     public int insertUser(User user);
 
+    public int deleteUser(Integer id);
+
+    public int updateUser(User user);
+
+    public List<User> findAll();
+
     public List<User> likeAll(String username);
+
+    public User findNestUser(Content content);
 }
--- mybatisTest/src/test/java/com/mybatis/BatisTest.java
+++ mybatisTest/src/test/java/com/mybatis/BatisTest.java
@@ -1,11 +1,14 @@
 package com.mybatis;
 
+import com.mybatis.domain.Content;
 import com.mybatis.domain.User;
 import com.mybatis.mapper.UserMapper;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -13,62 +16,104 @@ import java.io.InputStream;
 import java.util.List;
 
 public class BatisTest {
+    public SqlSession sqlSession;
+    public InputStream inputStream;
+    public UserMapper mapper;
 
-
-    @Test
-    public  void findAll() throws IOException {
+    @Before
+    public void init() throws IOException {
         String resource = "SqlMapConfig.xml";
-        InputStream inputStream = Resources.getResourceAsStream(resource);
+        inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
-        List<User> all = mapper.findAll();
-        for (User user : all) {
-            System.out.println(user);
-        }
-        inputStream.close();
-        sqlSession.close();
+        sqlSession = sqlSessionFactory.openSession();
+        mapper = sqlSession.getMapper(UserMapper.class);
     }
 
+    /**
+     * 增 并返回添加的id
+     * @throws IOException
+     */
     @Test
     public void insetUser() throws IOException {
-        String resource = "SqlMapConfig.xml";
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-
         User user1 = new User();
-        user1.setUsername("jacdkjjj");
-        user1.setPassword("1234556845");
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        user1.setUsername("jacdj");
+        user1.setPassword("123789");
+
         int all = mapper.insertUser(user1);
         System.out.println(all);
         sqlSession.commit();
         System.out.println(user1);
-
-        inputStream.close();
-        sqlSession.close();
-
     }
 
+    /**
+     * 删
+     */
+    @Test
+    public void deleetUser() throws IOException {
+        int all = mapper.deleteUser(18);
+        System.out.println(all);
+        sqlSession.commit();
+    }
+
+    /**
+     * 改
+     */
+    @Test
+    public void updateUser() throws IOException {
+
+        User user1 = new User();
+        user1.setId(15);
+        user1.setUsername("jackq");
+        user1.setPassword("123456");
+
+        int all = mapper.updateUser(user1);
+        System.out.println(all);
+        sqlSession.commit();
+    }
+
+
+    /**
+     * 查所有
+     * @throws IOException
+     */
+    @Test
+    public  void findAll() throws IOException {
+        List<User> all = mapper.findAll();
+        for (User user : all) {
+            System.out.println(user);
+        }
+    }
+
+    /**
+     * 模糊查询
+     * @throws IOException
+     */
     @Test
     public void likeAll() throws IOException {
-        String resource = "SqlMapConfig.xml";
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
-
 
         List<User> all = mapper.likeAll("j");
         sqlSession.commit();
         for (User user : all) {
             System.out.println(user);
         }
+    }
+
+    @Test
+    public void findNestUser(){
+        User user = new User();
+        user.setId(1);
+
+        Content content = new Content();
+        content.setUser(user);
+
+        User nestUser = mapper.findNestUser(content);
+
+        System.out.println(nestUser);
+    }
+
+    @After
+    public void end() throws IOException {
         inputStream.close();
         sqlSession.close();
     }
-
 }
