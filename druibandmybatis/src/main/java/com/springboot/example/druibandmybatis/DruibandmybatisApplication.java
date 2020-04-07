629d1fbb910fb4e186a474352f741b9d234cd87b
leilvlong <1455013965@qq.com>
2020-04-07 23:48:35 +0800

    mybatis + 自己实现 两种方式实现递归查询

--- druibandmybatis/src/main/java/com/springboot/example/druibandmybatis/DruibandmybatisApplication.java
+++ druibandmybatis/src/main/java/com/springboot/example/druibandmybatis/DruibandmybatisApplication.java
@@ -1,7 +1,10 @@
 package com.springboot.example.druibandmybatis;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DruibandmybatisApplication {
@@ -10,4 +13,9 @@ public class DruibandmybatisApplication {
         SpringApplication.run(DruibandmybatisApplication.class, args);
     }
 
+    @Bean
+    @ConfigurationProperties("spring.datasource.druid")
+    public DruidDataSource druidDataSource(){
+        return new DruidDataSource();
+    }
 }
